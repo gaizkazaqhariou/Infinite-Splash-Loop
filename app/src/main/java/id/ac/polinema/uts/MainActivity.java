e59e8a1e05805ca78e651c80b444f98c59a70479
gaizkazaqhariou <gaizkapolinema@gmail.com>
2020-04-02 09:55:09 +0700

    finishing commit

---
+++ app/src/main/java/id/ac/polinema/uts/MainActivity.java
@@ -0,0 +1,31 @@
+package id.ac.polinema.uts;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int lama = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home = new Intent(MainActivity.this, SplashActivity.class);
+                startActivity(home);
+                finish();
+            }
+        }, lama);
+    }
+}
